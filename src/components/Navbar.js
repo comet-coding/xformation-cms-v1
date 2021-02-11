e4a6d385cc05666550a462694e82d8c853b58280
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2021-02-11 12:18:04 +0530

    Conf file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -55,7 +55,7 @@ const Navbar = class extends React.Component {
 							<Link className='navbar-item' to='/about'>
 								About
 							</Link>
-							<Link className='navbar-item' to='/product'>
+							<Link className='navbar-item' to='/products'>
 								Products
 							</Link>
 							<Link className='navbar-item' to='/blog'>
